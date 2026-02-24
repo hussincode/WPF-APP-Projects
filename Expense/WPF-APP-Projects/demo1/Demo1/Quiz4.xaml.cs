5491859b5550bc858a19c92a22540f37ad300b5e
hussin <hussinhesham2099@gmail.com>
2026-02-24 06:15:16 +0200

    add Expense WPF project But without database

---
+++ Expense/WPF-APP-Projects/demo1/Demo1/Quiz4.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo1
+{
+    /// <summary>
+    /// Interaction logic for Quiz4.xaml
+    /// </summary>
+    public partial class Quiz4 : Page
+    {
+        public Quiz4()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            final f = new final();
+            this.NavigationService.Navigate(f);
+        }
+    }
+}
